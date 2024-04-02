bb9817aa31d5ea23346a9a0a3afe07371dbfa26b
Rahul Choudhari <59331583+rhulcodek@users.noreply.github.com>
2024-04-02 12:05:27 +0000

    done all

---
+++ 01_basic/02_basic.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+// alert("Rahul") //we are using node js not bowser
+
+let name="Rahul"
+let age= 26
+let isloggedIn=false
+let state=null;
+
+//number => 2 to power 53
+//bigInt 
+//string=>"",''
+//boolean => true/false
+//null => standAllone value
+//undefine =>  not value assign
+//symbol => uniques
+
+//object
+
+console.log(typeof state)//null data type in object
+
--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+const accountId=1022;
+let accountEmail="r@gmail.com"
+var password="1234"
+accountCity="Nagpur"
+let accountState
+
+
+// accountId=123 //not allowed
+accountEmail="gfjhdgf"
+password="34624"
+accountCity="hdjgjfg"
+
+console.log(accountId)
+console.table([accountId,accountEmail,password,accountCity,accountState])
